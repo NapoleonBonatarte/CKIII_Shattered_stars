2b5e234e963f05ee7c65a4d952720175bb122240
Tyler Windemuth <90519535+NapoleonBonatarte@users.noreply.github.com>
2022-04-27 09:27:03 -0700

    Add files via upload
    Creates colors for baronies and for counties (input { 0 1 0 } for barony and { 1 0 0 } for county).

--- create_colors.py
+++ create_colors.py
@@ -7,12 +7,21 @@ data = file.readlines()
 to_write = open('end_file', 'w')
 for i in data:
     line = i
+    # creates colors for baronies
     if line.strip() == 'color = { 0 1 0 }':
         print('hello')
         output1 = random.randint(0,255)
         output2 = random.randint(0,255)
         output3 = random.randint(0,255)
         output = '\t\t\t\t\tcolor = { ' + str(output1) +' ' + str(output2) + ' ' + str(output3) + ' }\n'
+        print(output)
+        to_write.write(output)
+    # creates color for counties
+    elif line.strip() == 'color = { 1 0 0 }':
+        output1 = random.randint(0,255)
+        output2 = random.randint(0,255)
+        output3 = random.randint(0,255)
+        output = '\t\t\t\tcolor = { ' + str(output1) +' ' + str(output2) + ' ' + str(output3) + ' }\n'
         print(output)
         to_write.write(output)
     else:
