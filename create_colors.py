bdb0dec1f6b00f179c4cdfbd15b2dfd96c311f23
Tyler Windemuth <90519535+NapoleonBonatarte@users.noreply.github.com>
2022-04-27 09:19:25 -0700

    Add files via upload
    This program creates province colors for baronies automatically

---
+++ create_colors.py
@@ -0,0 +1,24 @@
+import random
+
+file_path = ''
+
+file = open(file_path, 'r')
+data = file.readlines()
+to_write = open('end_file', 'w')
+for i in data:
+    line = i
+    if line.strip() == 'color = { 0 1 0 }':
+        print('hello')
+        output1 = random.randint(0,255)
+        output2 = random.randint(0,255)
+        output3 = random.randint(0,255)
+        output = '\t\t\t\t\tcolor = { ' + str(output1) +' ' + str(output2) + ' ' + str(output3) + ' }\n'
+        print(output)
+        to_write.write(output)
+    else:
+        to_write.write(line)
+
+
+
+file.close()
+to_write.close()
